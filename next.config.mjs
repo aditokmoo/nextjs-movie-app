1888dd3399c606e8c99444afb29bed10ebca5e66
Havock48 <adit.github@gmail.com>
2024-05-07 13:39:03 +0200

    redirect added in configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/tv-shows',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
